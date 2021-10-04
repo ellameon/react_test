8898008854edc10c56d15dc3a9a8e77495e61170
milyukov <9ellameon9@mail.ru>
2021-10-04 12:43:46 +0300

    18 lesson

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import * as https from "https";
 import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Profile from "./components/Profile/Profile.jsx";
-
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
     return (
-        <div className= 'app-wrapper'>
-            <Header />
-            <Navbar />
-            <Profile />
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            {/* <Profile />*/}
+            <div className='app-wrapper-content'>
+            <Dialogs/>
+            </div>
         </div>
     )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./My Posts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div><img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg'/>
             </div>
             <div>
