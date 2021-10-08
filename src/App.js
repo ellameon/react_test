d6aedac270d5e2d418cad85c5876dcb165ae3eec
milyukov <9ellameon9@mail.ru>
2021-10-08 10:22:34 +0300

    41 lesson

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
-import store, {addPost} from "./Redux/state";
+
 
 
 const App = (props) => {
