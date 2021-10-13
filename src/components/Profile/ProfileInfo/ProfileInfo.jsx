57973868cc9eea76246f019e99ff903c2dca3da6
milyukov <9ellameon9@mail.ru>
2021-10-13 08:51:04 +0300

    60 lesson

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,7 +10,7 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'/>
+                <img className={s.wall}  src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'/>
             </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
